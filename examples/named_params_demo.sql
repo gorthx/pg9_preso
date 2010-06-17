60e052fbcef6beabeb884546c3e4f1d6ec0b123f
gabrielle <gorthx@gmail.com>
2010-06-17 14:59:54 -0700

    update prior to PDXPUG presentation
    - updated syntax in example of function with named params (it's changed since alpha)
    - added example of passwordcheck

--- examples/named_params_demo.sql
+++ examples/named_params_demo.sql
@@ -1,3 +1,4 @@
+DROP FUNCTION bit_rate (bits_in int, secs int);
 CREATE OR REPLACE FUNCTION bit_rate (int, int)
 RETURNS int
 AS $$
@@ -9,6 +10,7 @@ SELECT bit_rate(3456789, 300);
 --time in seconds, bit in...OOPS.
 SELECT bit_rate(300, 3456789);
 
+DROP FUNCTION bit_rate (int, int);
 CREATE OR REPLACE FUNCTION bit_rate (bits_in int, secs int)
 RETURNS int
 AS $$
@@ -17,5 +19,5 @@ $$
 LANGUAGE SQL;
 
 SELECT bit_rate(3456789, 300);
-SELECT bit_rate(3456789 AS bits_in, 300 AS secs);
-SELECT bit_rate(300 AS secs, 3456789 AS bits_in);
+SELECT bit_rate(bits_in := 3456789, secs := 300);
+SELECT bit_rate(secs := 300, bits_in := 3456789);
---
+++ examples/passwordcheck_demo.sql
@@ -0,0 +1,3 @@
+CREATE ROLE markwkm WITH PASSWORD 'help';
+CREATE ROLE markwkm WITH PASSWORD 'potsticker';
+CREATE ROLE markwkm WITH PASSWORD 'potst1cker';
